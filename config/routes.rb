739e7ae78018dc289bee387d1a815df4b84261cf
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-02-23 19:52:03 -0800

    created root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
+
   resources :users, only: [:create, :edit, :update]
   get '/signup' => 'users#new', as: :signup
   # The priority is based upon order of creation: first created -> highest priority.
