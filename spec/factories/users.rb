3a102849a8472ae0cd0ffa92431863b146ccff9d
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-02-23 18:40:55 -0800

    created user model

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :user do
+    first "MyString"
+last "MyString"
+email "MyString"
+address "MyString"
+phone_number "MyString"
+password_digest "MyString"
+  end
+
+end
