2ed6a02f7fc3e7e4e3380fb92e86c97a00343353
Jonathan Thomas <Jonathanth0mas@yahoo.com>
2015-02-23 19:29:59 -0800

    sessions controller added

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  def create
+    user = User.where(email: params[:login][:email]).first
+    if user && user.authenticate(params[:login][:password])
+      log_in(user)
+      if user.admin == true
+        redirect_to pickups_path(user)
+      else
+       redirect_to user_path(user)
+      end
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end
+end
