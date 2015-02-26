bccdb1ad0f5d1c1e319b23b098d1fb5491f98126
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-02-25 18:00:58 -0800

    working login and nav

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.where(email: params[:login][:email]).first
     if user && user.authenticate(params[:login][:password])
       log_in(user)
+      redirect_to root_path
       # if user.admin == true
       #   redirect_to pickups_path(user)
       # else
