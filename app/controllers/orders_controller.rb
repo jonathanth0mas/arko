0bdfddfc043de93085ff05540c72364d5fccd74a
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-03-05 16:06:58 -0800

    made your orders appear

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,10 @@ class OrdersController < ApplicationController
 
 	def index
 		@orders = Order.all
-
 	end
 
 	def complete
+		@order_items = OrderItem.where(order_id: current_order.id)
 		@order = Order.find(current_order.id)
 		@order.update_attribute(:confirmed, true)
 	end
