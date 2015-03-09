4e95f86993de11ef4d68aa5cf11e7aa18359c0c3
Jonathan Thomas <Jonathanth0mas@yahoo.com>
2015-03-09 15:13:10 -0700

    added admin stuff and a little bit of complete order view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,8 @@ class ItemsController < ApplicationController
 		@items = Item.where(dish: params[:dish])
 		@order_item = current_order.order_items.new
 	end
+
+  def update
+    @item = Item.find(params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306002229) do
+ActiveRecord::Schema.define(version: 20150309215323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,8 +61,9 @@ ActiveRecord::Schema.define(version: 20150306002229) do
     t.string   "address"
     t.string   "phone_number"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
   end
 
   add_foreign_key "order_items", "items"
