f7b18fa620043ad5d374f124292062da8b491dc2
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-03-16 14:12:33 -0700

    started items controller for update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,23 @@ class ItemsController < ApplicationController
 		@items = Item.where(dish: params[:dish])
 		@order_item = current_order.order_items.new
 	end
+
+	def edit
+		@items = Item.where(dish: params[:dish])
+	end
+
+	def update
+		@items = Item.where(dish: params[:dish])
+		if @items.update_attribute(item_params)
+			redirect_to items_path
+		else
+			render :edit
+		end
+	end
+
+	private
+
+	def item_params
+		params.require(:item).permit(:name, :small, :medium, :large, :each, :misc, :dish, :image, :half, :whole, :full)
+	end
 end
