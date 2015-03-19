d9b30cf588109094356e2d0a7294c301ccf805a9
Jonathan Thomas <Jonathanth0mas@yahoo.com>
2015-03-19 15:34:59 -0700

    added fornt page description seeds

--- db/migrate/20150224201149_create_items.rb
+++ db/migrate/20150224201149_create_items.rb
@@ -2,9 +2,12 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :name
-      t.float :small
-      t.float :medium
-      t.float :large
+      t.float :smallC
+      t.float :mediumC
+      t.float :largeC
+      t.float :smallT
+      t.float :mediumT
+      t.float :largeT
       t.float :each
       t.string :misc
       t.string :dish
