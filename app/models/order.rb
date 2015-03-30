814637b8e3191e3b7d277d55f9f9caa1ef7f1d25
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-03-30 14:23:34 -0700

    created the subtotal method in orders model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,19 +2,15 @@ class Order < ActiveRecord::Base
 
 	belongs_to :user
 	has_many :order_items, dependent: :destroy
-	# before_save :update_subtotal
+	before_save :update_subtotal
 
-	# def subtotal
-	#     order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
-	# end
+	def subtotal
+	    order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.size) : 0 }.sum
+	end
 
 	private
 	  
-	  # def set_order_status
-	  #   self.order_status_id = 1
-	  # end
-
-	  # def update_subtotal
-	  #   self[:subtotal] = subtotal
-	  # end
+	def update_subtotal
+    	self[:subtotal] = subtotal
+  	end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,6 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  
 end
