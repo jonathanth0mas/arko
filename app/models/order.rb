377affbb679401a8dcef8a0931879b2204e5f19d
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-04-14 14:23:00 -0700

    changed css

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,15 +2,15 @@ class Order < ActiveRecord::Base
 
 	belongs_to :user
 	has_many :order_items, dependent: :destroy
-	before_save :update_subtotal
+	# before_save :update_subtotal
 
-	def subtotal
-	    order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
-	end
+	# def subtotal
+	#     order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
+	# end
 
-	private
+	# private
 	  
-	def update_subtotal
-    	self[:subtotal] = subtotal
-  	end
+	# def update_subtotal
+ #    	self[:subtotal] = subtotal
+ #  	end
 end
