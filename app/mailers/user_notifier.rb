fc98487d7e9ce8e74a40e574e62f337591c81fcb
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-08-13 16:34:02 -0600

    set sengrid

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,10 +1,10 @@
 class UserNotifier < ApplicationMailer
 	include SendGrid	
-	default :from => 'any_from_address@example.com'
+	default :from => 'arkofoods@gmail.com'
 
 	  # send a signup email to the user, pass in the user object that   contains the user's email address
 	  def send_order_email
-	    mail( :to => @user.email,
-	    :subject => 'Thanks for signing up for our amazing app' )
+	    mail( :to => 'yangdongkevin@gmail.com'
+	    :subject => 'Order' )
 	  end
 end
